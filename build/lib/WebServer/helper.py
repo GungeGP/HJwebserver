1440317204b9b9609828eb5ce4d8344794c4e2fa
Miklas Gunge <gungegp@gmail.com>
2026-06-26 13:08:07 +0200

    Removed build folder

--- build/lib/WebServer/helper.py
+++
@@ -1,7 +0,0 @@
-import json
-
-def _send_json(request, status, payload):
-    request.send_response(status)
-    request.send_header('Content-Type', 'application/json')
-    request.end_headers()
-    request.wfile.write(json.dumps(payload).encode('utf-8'))
